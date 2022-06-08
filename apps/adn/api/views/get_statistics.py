e828594189260210b6e2f1fd6c8071352a26ec64
Lenin Garcia <lagm1290@gmail.com>
2022-06-08 05:18:28 -0500

    ratio improvement

--- apps/adn/api/views/get_statistics.py
+++ apps/adn/api/views/get_statistics.py
@@ -9,9 +9,12 @@ class Statistics(APIView):
     def get(self, request):
         queryset = DnaVerified.objects.all().aggregate(
             count_mutant_dna = Count('is_mutant', filter=Q(is_mutant=True)),
-            count_human_dna = Count('is_mutant', filter=Q(is_mutant=False)),
-            ratio=(1 / Count('is_mutant', filter=Q(is_mutant=True))/Count('is_mutant', filter=Q(is_mutant=False)))
-        )
+            count_human_dna = Count('is_mutant', filter=Q(is_mutant=False))
+           )
+
+        queryset.update({
+            'ratio':round(1/(queryset['count_human_dna']/queryset['count_mutant_dna']),2)
+        })
         return Response(queryset,status.HTTP_200_OK)
 
 
