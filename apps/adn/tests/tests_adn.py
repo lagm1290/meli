e70881d91135a20a44d88dccd4453b080898f82a
Lenin Garcia <lagm1290@gmail.com>
2022-06-08 10:11:32 -0500

    unitest2

--- apps/adn/tests/tests_adn.py
+++ apps/adn/tests/tests_adn.py
@@ -5,7 +5,29 @@ from rest_framework import status
 class TestSetUp(APITestCase):
 
     def setUp(self):
-        self.url = '/mutant/'
+        self.url = '/api/adn/mutant/'
+
+    def test_adn_mutante(self):
+        response = self.client.post(
+            self.url,
+            {
+                "dna": ["ATGCGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"]
+            },
+            format='json'
+
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_adn_human(self):
+        response = self.client.post(
+            self.url,
+            {
+                "dna": ["ATGCGA", "CAGTGC", "TTATTT", "AGACGG", "GCGTCA", "TCACTG"]
+            },
+            format='json'
+
+        )
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_adn_null(self):
         response = self.client.post(
@@ -16,7 +38,7 @@ class TestSetUp(APITestCase):
             format='json'
 
         )
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_adn_not_array(self):
         response = self.client.post(
@@ -27,18 +49,28 @@ class TestSetUp(APITestCase):
             format='json'
 
         )
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
-
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_adn_length(self):
         response = self.client.post(
             self.url,
             {
-                "dna": ""
+                "dna": ["ATGCGA"]
+
             },
             format='json'
 
         )
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
+    def test_adn_reference(self):
+        response = self.client.post(
+            self.url,
+            {
+                "dna": ["ATGRGA", "CAGTGC", "TTATGT", "AGAAGG", "CCCCTA", "TCACTG"]
 
+            },
+            format='json'
+
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
